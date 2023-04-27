d2f700a4795494290206f56924effa08c256a91d
4xtc <44xtc44@gmail.com>
2023-04-27 23:40:33 +0200

    bruteforce db for finish task

---
+++ eisenmp_examples/utils_exa/db_exa_schema.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS exa;
+
+DROP TABLE IF EXISTS eisen_intern;
+
+CREATE TABLE "exa" (
+	"id"	INTEGER NOT NULL,
+	"created"	TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	"title"	TEXT NOT NULL,
+	"col_1" TEXT,
+	"col_2" TEXT,
+	"col_3" TEXT,
+	"col_4" TEXT,
+	"col_5" TEXT,
+	"col_6" TEXT,
+	PRIMARY KEY("id" AUTOINCREMENT)
+)
+;
