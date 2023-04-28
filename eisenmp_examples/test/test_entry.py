782e60c2edf111750740d92f3071754a1c8ed5fd
4xtc <44xtc44@gmail.com>
2023-04-28 20:49:38 +0200

    add delay

--- eisenmp_examples/test/test_entry.py
+++ eisenmp_examples/test/test_entry.py
@@ -1,5 +1,5 @@
+import time
 import unittest
-from unittest import mock
 import multiprocessing as mp
 
 import eisenmp_examples.eisenmp_exa_entry as entry
@@ -9,15 +9,6 @@ import eisenmp_examples.utils_exa.eisenmp_utils as utils
 class TestEntry(unittest.TestCase):
     """
     """
-
-    def _setUp(self):
-        """ 49152 to 65535 free
-        """
-        self.http_port = entry.serverPort
-        self.mock_http_port = mock.patch.object(
-            entry, 'serverPort', return_value=50_001
-        )
-
     def test_http_response(self):
         """
         """
@@ -27,15 +18,11 @@ class TestEntry(unittest.TestCase):
 
         msg = com_q.get()
         if msg == b'ready':
-            # with self.mock_http_port:
+            time.sleep(.2)
             response = utils.load_url('http://localhost:12321')
             str_b = response.read()
             assert b'font-family' in str_b
-        proc.terminate()
+
         proc.kill()
         proc.join()
 
-    def _tearDown(self):
-        """ org. port set back, for whatever reason
-        """
-        entry.serverPort = self.http_port
