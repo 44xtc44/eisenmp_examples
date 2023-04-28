f6a53300a9a36b37e8be1269c079d3dacf345e60
4xtc <44xtc44@gmail.com>
2023-04-28 18:01:33 +0200

    mock http port 50_001

--- eisenmp_examples/test/test_entry.py
+++ eisenmp_examples/test/test_entry.py
@@ -1,4 +1,5 @@
 import unittest
+from unittest import mock
 import multiprocessing as mp
 
 import eisenmp_examples.eisenmp_exa_entry as entry
@@ -8,14 +9,29 @@ import eisenmp_examples.utils_exa.eisenmp_utils as utils
 class TestEntry(unittest.TestCase):
     """
     """
+
+    def setUp(self):
+        """ 49152 to 65535 free
+        """
+        self.http_port = entry.serverPort
+        self.mock_http_port = mock.patch.object(
+            entry, 'serverPort', return_value=50_001
+        )
+
     def test_http_response(self):
         """
-        add terminate(), GitHub actions runs tox forever in Win
         """
         proc = mp.Process(target=entry.run_http)
         proc.start()
-        response = utils.load_url('http://localhost:12321')
-        str_b = response.read()
-        assert b'font-family' in str_b
+        with self.mock_http_port:
+            response = utils.load_url('http://localhost:50001')
+            str_b = response.read()
+            assert b'font-family' in str_b
         proc.terminate()
         proc.kill()
+        proc.join()
+
+    def tearDown(self):
+        """ org. port set back, for whatever reason
+        """
+        entry.serverPort = self.http_port
